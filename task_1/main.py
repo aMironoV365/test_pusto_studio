3eed5f0ea1bba27fa8b27fca09da38ac4e902e97
Andrey <A.mironov95@mail.ru>
2024-10-02 21:34:38 +0300

    task done

--- task_1/main.py
+++ task_1/main.py
@@ -7,19 +7,18 @@ Session = sessionmaker(bind=engine, expire_on_commit=False)
 session = Session()
 
 if __name__ == '__main__':
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
-
-    player = Player(username="andrey")
-
-    boost = Boost(boost_type="Усиление", description="Добыча золота увеличена на 20%")
-
-    session.add(player)
-    session.add(boost)
-    session.commit()
-    session.close()
-
     with Session() as ses:
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+
+        player = Player(username="andrey")
+
+        boost = Boost(boost_type="Усиление", description="Добыча золота увеличена на 20%")
+
+        session.add(player)
+        session.add(boost)
+        session.commit()
+
         player = ses.execute(select(Player).where(Player.username == "andrey")).scalars().first()
         boost = ses.execute(select(Boost).where(Boost.boost_type == "Усиление")).scalars().first()
         player.login()
